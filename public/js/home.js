7571c7f95e505a79078ae8f7e8fdc17e5191b184
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2021-03-22 23:58:31 +0200

    about section done

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,15 @@
+var introSectionTopOffset = $("#aboutSection").offset().top;
 var aboutSectionTopOffset = $("#aboutSection").offset().top;
 var componentSectionTopOffset = $("#productComponent").offset().top;
 
-$(window).scroll(() => {
-    animateOnView($(".fade-section *"), aboutSectionTopOffset);
-    animateOnView($("#productComponent *"), componentSectionTopOffset);
+$(window).ready(() => {
+
+    animateOnView($(".intro-section *"), introSectionTopOffset);
+
+    $(window).scroll(() => {
+        animateOnView($(".intro-section *"), introSectionTopOffset);
+        animateOnView($(".fade-section *"), aboutSectionTopOffset);
+        animateOnView($("#productComponent *"), componentSectionTopOffset);
+    });
+
 });
